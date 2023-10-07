d38d11a9ece820bafa697ffaf6226a88855ccb23
PiotrRogulski <piter.rogulski@gmail.com>
2023-10-07 18:15:14 +0200

    Don't maximize window on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sfsw_lab_1_spring/app.dart';
-import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-  await windowManager.maximize();
 
   runApp(const App());
 }
