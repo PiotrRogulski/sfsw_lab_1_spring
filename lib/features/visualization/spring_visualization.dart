01740b41a3d3ff032b897254c13124fb82353a52
PiotrRogulski <piter.rogulski@gmail.com>
2023-10-18 23:22:47 +0200

    Add basic spring visualization

--- lib/features/visualization/spring_visualization.dart
+++ lib/features/visualization/spring_visualization.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:sfsw_lab_1_spring/features/simulation/observation.dart';
+import 'package:sfsw_lab_1_spring/features/simulation/spring_simulation_store.dart';
 import 'package:sfsw_lab_1_spring/layouts/layout_slot.dart';
 
 class SpringVisualization extends LayoutSlot {
@@ -9,11 +13,28 @@ class SpringVisualization extends LayoutSlot {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.transparent,
-      child: Center(
-        child: Text('Spring visualization'),
-      ),
+    final simulationStore = context.read<SpringSimulationStore>();
+
+    return Observer(
+      builder: (context) {
+        final currentReading = simulationStore.latestReading;
+
+        return Placeholder(
+          color: Colors.transparent,
+          child: Center(
+            child: switch (currentReading) {
+              null => const Text('Spring visualization'),
+              // TODO: nicer spring visualization
+              Observation(:final position) => Slider(
+                  min: -10,
+                  max: 10,
+                  value: position,
+                  onChanged: null,
+                ),
+            },
+          ),
+        );
+      },
     );
   }
 }
