7ac14fd6c9309c0e8d9510fa0e811cb8fcb6506c
PiotrRogulski <piter.rogulski@gmail.com>
2023-10-20 23:32:07 +0200

    Increase spring visualization bounds

--- lib/features/visualization/spring_visualization.dart
+++ lib/features/visualization/spring_visualization.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
+import 'package:sfsw_lab_1_spring/features/parameters/parameters_store.dart';
 import 'package:sfsw_lab_1_spring/features/simulation/observation.dart';
 import 'package:sfsw_lab_1_spring/features/simulation/spring_simulation_store.dart';
 import 'package:sfsw_lab_1_spring/layouts/layout_slot.dart';
@@ -14,10 +15,13 @@ class SpringVisualization extends LayoutSlot {
   @override
   Widget build(BuildContext context) {
     final simulationStore = context.read<SpringSimulationStore>();
+    final parametersStore = context.read<ParametersStore>();
 
     return Observer(
       builder: (context) {
         final currentReading = simulationStore.latestReading;
+
+        final (:min, :max) = parametersStore.initialPosition.bounds;
 
         return Placeholder(
           color: Colors.transparent,
@@ -26,8 +30,8 @@ class SpringVisualization extends LayoutSlot {
               null => const Text('Spring visualization'),
               // TODO: nicer spring visualization
               Observation(:final position) => Slider(
-                  min: -10,
-                  max: 10,
+                  min: 2 * min,
+                  max: 2 * max,
                   value: position,
                   onChanged: null,
                 ),
