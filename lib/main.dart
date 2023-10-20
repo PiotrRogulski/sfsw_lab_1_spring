06e245d4956bb79dcc833f3045fe4273a70e7c7b
PiotrRogulski <piter.rogulski@gmail.com>
2023-10-20 11:03:07 +0200

    Enable stringifying equatables in release

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:sfsw_lab_1_spring/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  EquatableConfig.stringify = true;
 
   runApp(const App());
 }
